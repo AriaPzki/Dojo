b097b2859b7a056ddd8ae0600efcbe32a970449e
AriaPzki <AriaPzki@gmail.com>
2023-06-13 11:43:57 -0400

    Starting Router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Create from "./components/Create";
 
 function App() {
   const title = "App Component";
 
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/create">
+              <Create />
+            </Route>
+          </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
